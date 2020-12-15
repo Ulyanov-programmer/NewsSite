28d177363ad8fa4e73b686b7bab0b4e1242b331f
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2020-12-15 13:35:46 +0400

    Добавлена модель представления для страницы добавления новости.
    Добавлен Post метод для NewsController, который отвечает за передачу данных из формы в другие части приложения.

--- WebApplication1/Controllers/NewsController.cs
+++ WebApplication1/Controllers/NewsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsSite.BL;
 using NewsSite.BL.Abstractions;
+using NewsSite.BL.DTOModels;
+using NewsSite.BL.Servies;
+using NewsSite.UI.ViewModels;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,17 +12,38 @@ namespace NewsSite.UI.Controllers
 {
     public class NewsController : Controller, IAppController
     {
+        public NewsSiteContext Context { get; }
+
+        public NewsController(NewsSiteContext context)
+        {
+            Context = context;
+        }
+
         public IActionResult Watch()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult AddNews()
         {
             return View();
         }
 
-        IActionResult IAppController.RedirectToHomePage()
+        [HttpPost]
+        public async Task<IActionResult> AddNews(AddNewsVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                IDTOModel newsDTO = new DTONews(model.NameOfNews, model.DocFile.FileName);
+                DBManager.AddEntity(Context, newsDTO);
+                await FileManager.SaveFile(model.DocFile);
+            }
+
+            return RedirectToHomePage();
+        }
+
+        IActionResult RedirectToHomePage()
         {
             return RedirectToAction("Index", "Home");
         }
---
+++ WebApplication1/ViewModels/AddNewsVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsSite.UI.ViewModels
+{
+    public class AddNewsVM
+    {
+        [Required(ErrorMessage = "Добавьте файл с содержимым новости!")]
+        public IFormFile DocFile { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Длина названия статьи должна быть от 5 до 30 символов!")]
+        public string NameOfNews { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя автора!")]
+        public string NameOfAuhtor { get; set; }
+    }
+}
