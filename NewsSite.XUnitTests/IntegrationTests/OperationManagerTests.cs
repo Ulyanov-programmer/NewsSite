743f7cdd1a39ee197840feeda32856def30f4702
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-01-29 13:01:24 +0400

    Refactoring of AddEntity_Test.
    Added regions.
    Deleted the GetDbContextOptions method.

--- NewsSite.XUnitTests/IntegrationTests/OperationManagerTests.cs
+++ NewsSite.XUnitTests/IntegrationTests/OperationManagerTests.cs
@@ -19,7 +19,6 @@ namespace NewsSite.Tests.IntegrationTests
         internal void AddEntity_Test(DbNews_Mosk dbNews_Mosk)
         {
             //Arrange
-            using var context = new NewsSiteContext(GetDbContextOptions());
             var operManager = new OperationManager();
 
             //Act
@@ -29,11 +28,17 @@ namespace NewsSite.Tests.IntegrationTests
             //Assert
             switch (dbNews_Mosk.InitVariant)
             {
+                #region GoodVariant
+
                 case InitializationVariants.Good:
 
                     Assert.True(dbNews_Mosk.MoskLog.Result,
                         "The MoskLog should Result.True because InitializationVariants is Good and the method must be executed.");
                     break;
+
+                #endregion
+
+                #region NullVariant
 
                 case InitializationVariants.Null:
 
@@ -41,43 +46,18 @@ namespace NewsSite.Tests.IntegrationTests
                         "The MoskLog should Result.False because InitializationVariants is Null and the method should not be executed.");
                     break;
 
+                #endregion
+
+                #region EmptyVariant
+
                 case InitializationVariants.Empty:
 
                     Assert.False(dbNews_Mosk.MoskLog.Result,
                         "The MoskLog should Result.False because InitializationVariants is Empty and the method should not be executed.");
                     break;
+
+                #endregion
             }
-        }
-
-        //TODO: Убрать копирование этого метода в нескольких классах.
-        private static DbContextOptions<NewsSiteContext> GetDbContextOptions()
-        {
-            /* Specifies the fully qualified path to the directory of the appsettings.json file.
-               The first argument is the directory where appsettings.json is located, 
-               the second is the full path to that directory. */
-
-            string pathToAppsettingsDir = Path.GetFullPath(@"NewsSite\WebApplication1\", AppDomain.CurrentDomain.BaseDirectory
-                                              .Remove(AppDomain.CurrentDomain.BaseDirectory.IndexOf("NewsSite")));
-            #region Easy to read version. 
-
-            //string basePath = AppDomain.CurrentDomain.BaseDirectory;
-            //basePath = basePath.Remove(basePath.IndexOf("NewsSite."));
-
-            //string relativePath = @"WebApplication1\";
-
-            //string pathToAppsettingsDir = Path.GetFullPath(relativePath, basePath);
-
-            #endregion
-
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(pathToAppsettingsDir)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-
-            return new DbContextOptionsBuilder<NewsSiteContext>()
-                  //Enter the connection string from appsettings.json below.
-                  .UseSqlServer(new SqlConnection(configuration.GetConnectionString("DefaultConnection"))).Options;
         }
     }
 }
