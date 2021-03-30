fa34fdd9f18f9fe3c65a5b88c730fa9fd3cc7df2
Ulyanov Ivan <ccoldatheinrich@yandex.ru>
2021-03-30 11:43:54 +0400

    Created and prepared a Blazor Server project.
    Already added HTML layouts, CSS / SCSS styles and view models. Configured Startup.

---
+++ NewsSite.BlazorUI/Data/AddNewsVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsSite.UI.ViewModels
+{
+    public class AddNewsVM
+    {
+        [Required(ErrorMessage = "Добавьте файл с содержимым новости!")]
+        public IFormFile DocFile { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Длина названия статьи должна быть от 5 до 30 символов!")]
+        public string NameOfNews { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя автора!")]
+        public string NameOfAuhtor { get; set; }
+    }
+}
